7b56904bbaf1f95ddcde4de01003c236e9b2c60f
blackwood-star <woodblack686@gmail.com>
2024-08-16 01:12:50 +0530

    typescript removed

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -11,22 +11,22 @@ export default function nav() {
           <div className="flex-1 md:flex hidden justify-center">
               <div className="navlist flex flex-row gap-14 justify-center">
                     <div className="navlistItem hover:text-indigo-500 relative group">
-                        <a href="#" className="inline-flex items-center gap-2">
+                        <a href="#platform" className="inline-flex items-center gap-2">
                             Platform
                             <svg className="pt-1" width="16" height="10" viewBox="0 0 16 10" fill="none" xmlns="http://www.w3.org/2000/svg">
                                 <path d="M15 1L8.25942 7.74058C7.84905 8.15095 7.1767 8.12697 6.79662 7.68841L1 1" stroke="black" stroke-width="2"/>
                             </svg>
                         </a>
                         <div className="absolute bg-white lg:w-[220px] shadow-lg rounded-lg py-5 hidden group-hover:block opacity-0 group-hover:opacity-100">
-                            <div className="navlistItem hover:text-indigo-500 px-5 pb-5"><a href="#">App Developement</a></div>
-                            <div className="navlistItem hover:text-indigo-500 px-5 pb-5"><a href="#">Product Developement</a></div>
-                            <div className="navlistItem hover:text-indigo-500 px-5"><a href="#">Digital Marketing</a></div>
+                            <div className="navlistItem hover:text-indigo-500 px-5 pb-5"><a href="#app-development">App Developement</a></div>
+                            <div className="navlistItem hover:text-indigo-500 px-5 pb-5"><a href="#product-development">Product Developement</a></div>
+                            <div className="navlistItem hover:text-indigo-500 px-5"><a href="#digital-marketing">Digital Marketing</a></div>
                         </div>
                     </div>
-                    <div className="navlistItem hover:text-indigo-500"><a href="#">Solutions</a></div>
-                    <div className="navlistItem hover:text-indigo-500"><a href="#">Pricing</a></div>
-                    <div className="navlistItem hover:text-indigo-500"><a href="#">Resources</a></div>
-                    <div className="navlistItem hover:text-indigo-500"><a href="#">Company</a></div>
+                    <div className="navlistItem hover:text-indigo-500"><a href="#solutions">Solutions</a></div>
+                    <div className="navlistItem hover:text-indigo-500"><a href="#pricings">Pricing</a></div>
+                    <div className="navlistItem hover:text-indigo-500"><a href="#resources">Resources</a></div>
+                    <div className="navlistItem hover:text-indigo-500"><a href="#company">Company</a></div>
               </div>
           </div>
           <div className="flex-1 h-full md:hidden flex">
@@ -46,8 +46,8 @@ export default function nav() {
           </div>
           { /* contact */ }
           <div className="md:flex hidden flex-row justify-end  gap-2">
-              <button className="px-7 py-2 contact_D"><a href="">Log In</a></button>
-              <button className="px-7 py-2 contact_D border-2 rounded-full font-[500]"><a href="">Book a demo</a></button>
+              <button className="px-7 py-2 contact_D"><a href="log-in">Log In</a></button>
+              <button className="px-7 py-2 contact_D border-2 rounded-full font-[500]"><a href="book-demo">Book a demo</a></button>
           </div>
       </div>  
     </nav>
